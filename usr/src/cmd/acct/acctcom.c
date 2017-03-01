5acd7e40cadca1e15f7e0c69a6a1749263db2015
Hayashi Naoyuki <alpha@culzean.org>
2017-03-02 02:00:06 +0900

    compare dev32_t value and dev_t value.

--- usr/src/cmd/acct/acctcom.c
+++ usr/src/cmd/acct/acctcom.c
@@ -23,6 +23,9 @@
  * Copyright 2005 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
+/*
+ * Copyright 2017 Hayashi Naoyuki
+ */
 
 /*	Copyright (c) 1984, 1986, 1987, 1988, 1989 AT&T	*/
 /*	  All Rights Reserved  	*/
@@ -559,7 +562,7 @@ println(void)
 	    a->ac_tty != 0xffff? devtolin(a->ac_tty):"?");
 #else
 	fprintf(stdout, " %-*.*s", OUTPUT_LSZ, OUTPUT_LSZ,
-	    a->ac_tty != (dev_t)-1? devtolin(a->ac_tty):"?");
+	    a->ac_tty != (dev32_t)-1? devtolin(a->ac_tty):"?");
 #endif
 	t = a->ac_btime;
 	cftime(time_buf, DATE_FMT1, &t);
